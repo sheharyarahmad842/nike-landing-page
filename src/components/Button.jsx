b33e631df9f141e5fdfcce0e421f1a7ecf1c7142
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2023-09-28 16:16:27 +0500

    complete special offers section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-const Button = ({ label, icon }) => {
+const Button = ({ label, icon, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className='flex justify-center items-center gap-2 font-montserrat leading-none bg-coral-red text-white px-6 py-3 rounded-full border border-coral-red hover:opacity-95'>
+    <button
+      className={`flex justify-center items-center gap-2 font-montserrat leading-none ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red text-white border-coral-red'
+      }  px-6 py-3 rounded-full border-2 hover:opacity-95`}
+    >
       {label}
       {icon && (
         <img
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,5 +1,37 @@
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+import { offer } from '../assets/images';
+
 const SpecialOffers = () => {
-  return <div>SpecialOffers</div>;
+  return (
+    <section className='max-container'>
+      <h2 className='font-palanquin text-4xl leading-normal'>
+        Special <span className='text-coral-red'>Offers</span>
+      </h2>
+      <p className='mt-4 info-text'>
+        Embark on a shopping journey that redefines your experience with
+        unbeatable deals. From premier selections to incredible savings, we
+        offer unparalleled value that sets us apart.
+      </p>
+      <p className='mt-6 info-text'>
+        Navigate a realm of possibilities designed to fulfill your unique
+        desires, surpassing the loftiest expectations. Your journey with us is
+        nothing short of exceptional.
+      </p>
+      <div className='flex gap-4 mt-6'>
+        <Button icon={arrowRight} label='Shop Now' />
+        <Button
+          label='Learn More'
+          backgroundColor='bg-white'
+          textColor='text-slate-gray'
+          borderColor='text-slate-gray'
+        />
+      </div>
+      <div className='flex justify-center items-center mt-8'>
+        <img src={offer} alt='' className='w-773 h-687 object-contain' />
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffers;
